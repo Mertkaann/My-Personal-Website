d2da4e47be3d05a70e499df1a93e310ce3378560
ituitis-gulk19 <gulk19@itu.edu.tr>
2019-12-25 23:13:57 +0300

    Adapted the web site made in assignment1 as a bottle web application.

--- server.py
+++ server.py
@@ -1,15 +1,21 @@
 import os
 
 from bottle import Bottle
-
+from pathlib import Path
 
 def home_page():
-    return "Assignment 2 Hello World"
-
+    return Path("index.html").read_text()
+def education_page():
+    return Path("education.html").read_text()
+def extras_page():
+    return Path("extras.html").read_text()
 
 def create_app():
     app = Bottle()
     app.route("/", "GET", home_page)
+    app.route("/index.html", "GET", home_page)
+    app.route("/education.html", "GET", education_page)
+    app.route("/extras.html", "GET", extras_page)
     return app
 
 
