7dc47c5d09beb411b4517bb6f6903daedb1df789
ituitis-gulk19 <gulk19@itu.edu.tr>
2019-12-25 22:31:56 +0300

    Improved  Repl_comments.py by adding Password Security to add new comment

--- repl_comments.py
+++ repl_comments.py
@@ -1,18 +1,29 @@
+#Program To Print Previous comments entered. Secured with Password.
 
-#Simple Program To Print Previous comments entered
-comments = list()
+from hashlib import sha256
 
+def create_hash(password):
+    pw_bytestring = password.encode()
+    return sha256(pw_bytestring).hexdigest()
 def printComments():
-    for cmnd in comments:
-        print(cmnd)
+    print("Previously entered comments:")
+    for i,cmnd in enumerate(comments):
+        print(i+1,'.',cmnd)
+
+comments = list()
+pw_hash = "55f96f7d175068fba8700315b5849c1ffbb1d037e64bccb69abc81e8baf64416"
 
 while True:
-    comment = input('Enter Your Input [Press q to leave ] ')
+    comment = input('Enter Your Comment [Press q to leave ] ')
     if comment == 'q' or comment == 'Q':
         break
-    
-    comments.append(comment)
-    printComments()
+    pw_confirmation = create_hash(input('Enter Your Password: '))
+    if pw_hash != pw_confirmation:
+        print("I a sorry I can’t let you do that.")
+        continue
+    else:
+        comments.append(comment)
+        printComments()
     
 
 print("Program Terminated Succesfuly ")
