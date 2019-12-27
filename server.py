ded8f3f13d9a21010d97aa23353134c146363f5e
ituitis-gulk19 <gulk19@itu.edu.tr>
2019-12-28 01:03:46 +0300

    Added Table page includes simple table with using bottle template

--- server.py
+++ server.py
@@ -1,19 +1,17 @@
 import os
 
-from bottle import Bottle, route, run, static_file
+from bottle import Bottle, route, run, static_file, request,SimpleTemplate,template
 from pathlib import Path
+import pandas as pd
 
 PATH = os.getcwd()
-print(PATH+"/static")
+
+
 @route('/')
 @route('/index.html')
 def home_page():
     return Path("index.html").read_text()
 
-
-@route('/login')
-def login():
-    return "<h1>On Login Page </h1>"
 @route('/education.html')
 def education_page():
     return Path("education.html").read_text()
@@ -24,7 +22,18 @@ def extras_page():
 
 @route('/static/<filename:path>')
 def send_static(filename):
-    print("path is: ",PATH)
     return static_file(filename, root= os.path.join(PATH,"static"))
 
-run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
+@route('/table')
+@route('/page.html')
+def table():
+    client_ip = request.environ.get('HTTP_X_FORWARDED_FOR') or request.environ.get('REMOTE_ADDR')
+    output = 'Your IP is: {}\n'.format(client_ip)
+    # do something to create a pandas datatable
+    df = pd.DataFrame(data=[[1,2],[3,4]],columns = ["IP Address","Count"])
+    df_html = df.to_html()  # use pandas method to auto generate html
+    return template('page',table_html=df_html,output=output)
+
+
+run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)),reloader=True)
