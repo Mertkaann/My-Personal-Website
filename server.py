ecae6a1d51168d6815206ea8cd5f0e51d21409e1
ituitis-gulk19 <gulk19@itu.edu.tr>
2019-12-27 21:53:51 +0300

    Added Css and local images to website.

--- server.py
+++ server.py
@@ -1,23 +1,30 @@
 import os
 
-from bottle import Bottle
+from bottle import Bottle, route, run, static_file
 from pathlib import Path
 
+PATH = os.getcwd()
+print(PATH+"/static")
+@route('/')
+@route('/index.html')
 def home_page():
     return Path("index.html").read_text()
+
+
+@route('/login')
+def login():
+    return "<h1>On Login Page </h1>"
+@route('/education.html')
 def education_page():
     return Path("education.html").read_text()
+
+@route('/extras.html', methods=('GET'))
 def extras_page():
     return Path("extras.html").read_text()
 
-def create_app():
-    app = Bottle()
-    app.route("/", "GET", home_page)
-    app.route("/index.html", "GET", home_page)
-    app.route("/education.html", "GET", education_page)
-    app.route("/extras.html", "GET", extras_page)
-    return app
+@route('/static/<filename:path>')
+def send_static(filename):
+    print("path is: ",PATH)
+    return static_file(filename, root= os.path.join(PATH,"static"))
 
-
-application = create_app()
-application.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
