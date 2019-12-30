dda9ce8492491f94da178bb2545d79c82f84a709
ituitis-gulk19 <gulk19@itu.edu.tr>
2019-12-30 23:17:04 +0300

    added reset button functionality to IP table.

--- repl_comments.py
+++ repl_comments.py
@@ -5,26 +5,31 @@ from hashlib import sha256
 def create_hash(password):
     pw_bytestring = password.encode()
     return sha256(pw_bytestring).hexdigest()
-def printComments():
+def printComments(comments):
     print("Previously entered comments:")
     for i,cmnd in enumerate(comments):
         print(i+1,'.',cmnd)
+def get_pwhash():
+    return "55f96f7d175068fba8700315b5849c1ffbb1d037e64bccb69abc81e8baf64416"
+def main():
+    comments = list()
+    pw_hash = get_pwhash()
 
-comments = list()
-pw_hash = "55f96f7d175068fba8700315b5849c1ffbb1d037e64bccb69abc81e8baf64416"
-
-while True:
-    comment = input('Enter Your Comment [write terminate to leave ] ')
-    comment = comment.lower()
-    if comment == 'terminate':
-        break
-    pw_confirmation = create_hash(input('Enter Your Password: '))
-    if pw_hash != pw_confirmation:
-        print("I a sorry I can’t let you do that.")
-        continue
-    else:
-        comments.append(comment)
-        printComments()
+    while True:
+        comment = input('Enter Your Comment [write terminate to leave ] ')
+        comment = comment.lower()
+        if comment == 'terminate':
+            break
+        pw_confirmation = create_hash(input('Enter Your Password: '))
+        if pw_hash != pw_confirmation:
+            print("I a sorry I can’t let you do that.")
+            continue
+        else:
+            comments.append(comment)
+            printComments(comments)
     
 
-print("Program Terminated Succesfuly ")
+    print("Program Terminated Succesfuly ")
+
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -4,10 +4,13 @@ from bottle import Bottle, route, run, static_file, request,SimpleTemplate,templ
 from pathlib import Path
 import pandas as pd
 from collections import defaultdict
+from repl_comments import create_hash,get_pwhash
+
 
 PATH = os.getcwd()
-
-Ip_list = defaultdict(int)
+pw_hash = get_pwhash()
+total_list = defaultdict(int)
+new_list = defaultdict(int)
 
 
 @route('/index.html')
@@ -31,14 +34,35 @@ def send_static(filename):
 @route('/table')
 @route('/page.html')
 def table():
-    Ip_list = loadIPs()
+    
     client_ip = request.environ.get('HTTP_X_FORWARDED_FOR') or request.environ.get('REMOTE_ADDR')
     output = '\n\nYour IP is: {}\n'.format(client_ip)
-    Ip_list[client_ip] += 1
+    new_list[client_ip] += 1
 
-    df = pd.DataFrame(list(zip(Ip_list.keys(),Ip_list.values())),columns = ["IP Address","Count"])
+    df = gettable()
     df_html = df.to_html()
     return template('page',table_html=df_html,output=output)
+
+@route('/form', method='POST')
+def resetIPs():
+    pw = request.forms.get('password')
+    pw_confirmation = create_hash(pw)
+    if pw_hash == pw_confirmation:
+        saveIPs()
+        return '<p>Your login information was correct.</p><a href="page.html" class="btn">Reset Page</a>'
+    else:
+        return '<p>Login failed.</p><a href="page.html" class="btn">Reset Page</a>'
+def gettable():
+    data = defaultdict(int)
+    for ip in total_list:
+        data[ip] = total_list[ip] + new_list[ip]
+
+    for ip in new_list.keys():
+        if ip not in total_list.keys():
+            data[ip] = new_list[ip]
+    
+    df = pd.DataFrame(list(zip(data.keys(),data.values())),columns = ["IP Address","Count"])
+    return df
 
 def printIPs():
     for ip in Ip_list:
@@ -50,21 +74,22 @@ def loadIPs():
         
         while line:
             ip,count = line.split(' ')
-            Ip_list[ip] = int(count)
+            total_list[ip] = int(count)
             line = f.readline()
-        return Ip_list
 def saveIPs():
+    loadIPs()
+    for ip1 in new_list:
+        total_list[ip1] += new_list[ip1]
+
     with open("IPadresses.txt", "w") as f:
-        for ip in Ip_list:
-            f.write(ip + ' ' + str(Ip_list[ip]) + '\n')
+        for ip in total_list:
+            f.write(ip + ' ' + str(total_list[ip]) + '\n')
+    total_list.clear()
+    new_list.clear()
 
 
-def addIP(Ip_list, client_ip):
     
-    while ip in Ip_list:
-        if(ip == client_ip):
-            Ip_list
-
-run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)),reloader=True)
-saveIPs()
-    
+if __name__ == "__main__":
+    loadIPs()
+    run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)),reloader=True)
+    saveIPs()
